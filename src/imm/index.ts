d38dd7615cb08fa42b4b9398850c55255923605b
strikerdlm <dlmalpica@me.com>
2026-05-20 18:20:25 -0500

    feat(imm): public barrel (Phase P0 surface)

---
+++ src/imm/index.ts
@@ -0,0 +1,5 @@
+// src/imm/index.ts
+export * from "./types";
+export { IMM_CONDITIONS } from "./conditions";
+export { loadIMMPriors, validatePriorsJson } from "./priors";
+export { IMM_KITS, computeRAF, customKit } from "./kits";
